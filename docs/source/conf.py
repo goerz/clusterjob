c0973473738ff5f24237f3f9b94777ca27dbe887
Michael Goerz <goerz@stanford.edu>
2015-12-28 22:11:19 -0800

    Import package in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,10 @@ import sys
 import os
 import shlex
 
+# We import the clusterjob just to ensure that it installed in the same
+# environment as sphinx, so that autdoc works
+import clusterjob
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
